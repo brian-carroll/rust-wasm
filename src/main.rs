e3047a2bfaff79fdad0afda3487e0780deadf6b8
Brian Carroll <brian.carroll.ireland@gmail.com>
2021-08-23 20:02:10 +0100

    Get rid of "anyhow" package and handle the errors explicitly

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,6 @@ extern crate parity_wasm;
 use parity_wasm::{builder, elements};
 use wasmer::{Store, Module, Instance, Value, imports};
 
-
 fn create_wasm_bytes() -> Vec<u8> {
     // Based on the example at https://github.com/paritytech/parity-wasm/blob/master/examples/build.rs
     let module = builder::module()
@@ -35,20 +34,39 @@ fn create_wasm_bytes() -> Vec<u8> {
 }
 
 
-fn main() -> anyhow::Result<()> {
+fn main() -> Result<(), String> {
     let wasm_bytes: Vec<u8> = create_wasm_bytes();
 
-    fs::write("test.wasm", &wasm_bytes)?;
+    match fs::write("test.wasm", &wasm_bytes) {
+        Ok(()) => (),
+        Err(e) => return Err(format!("writing Wasm file {:?}", e)),
+    };
 
     // Based on the example at https://docs.rs/wasmer/2.0.0/wasmer/
     let store = Store::default();
-    let module = Module::from_binary(&store, &wasm_bytes)?;
+    let module = match Module::from_binary(&store, &wasm_bytes) {
+        Ok(m) => m,
+        Err(e) => return Err(format!("loading Wasm module {:?}", e)),
+    };
+
     // The module doesn't import anything, so we create an empty import object.
     let import_object = imports! {};
-    let instance = Instance::new(&module, &import_object)?;
 
-    let add_one = instance.exports.get_function("add_one")?;
-    let result = add_one.call(&[Value::I32(42)])?;
+    let instance = match Instance::new(&module, &import_object) {
+        Ok(inst) => inst,
+        Err(e) => return Err(format!("instanciating Wasm module {:?}", e)),
+    };
+
+    let add_one = match instance.exports.get_function("add_one") {
+        Ok(f) => f,
+        Err(e) => return Err(format!("getting exported Wasm function {:?}", e)),
+    };
+
+    let result = match add_one.call(&[Value::I32(42)]) {
+        Ok(f) => f,
+        Err(e) => return Err(format!("calling export Wasm function {:?}", e)),
+    };
+
     assert_eq!(result[0], Value::I32(43));
 
     return Ok(());
